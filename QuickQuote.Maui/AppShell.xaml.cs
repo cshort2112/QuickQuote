beecfc901526537e35081faea6b62bc31670af47
cshort2112 <Collin@webment.net>
2025-11-13 19:21:26 -0700

    Init

---
+++ QuickQuote.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickQuote.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ QuickQuote.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace QuickQuote.Shared;
+
+public class Class1
+{
+}
